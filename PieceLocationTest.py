76b5fbd4b63712c9253897db3fd56a6fa79e352d
ruby <rarwxz@mail.umkc.edu>
2020-10-11 01:06:18 -0500

    Create PieceLocationTest.py

---
+++ PieceLocationTest.py
@@ -0,0 +1,24 @@
+
+import pygame
+
+def  Piece_Location(x, y, spots_Dict):
+    xy_range = 25
+
+    for i in spots_Dict.values():
+        if (i[0] > x - xy_range and i[0] < x + xy_range and i[1] > y - xy_range and i[1] < y + xy_range):
+            return True
+        else:
+            return False
+
+
+def Piece_Location_Test(spots_Dict):
+    xy_range = 25
+    
+    for x in range(800):
+        for y in range(800):
+            for i in spots_Dict.values():
+                if (i[0] > x - xy_range and i[0] < x + xy_range and i[1] > y - xy_range and i[1] < y + xy_range):
+                    assert Piece_Location(x, y, spots_Dict) == True
+                    
+    print("End")
+    return 
