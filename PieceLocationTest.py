a89cbf255724b485606188cb915b55d386810372
ruby <rarwxz@mail.umkc.edu>
2020-10-11 01:49:59 -0500

    Update PieceLocationTest.py

--- PieceLocationTest.py
+++ PieceLocationTest.py
@@ -1,5 +1,15 @@
 
 import pygame
+from ninemm.py import *
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+    while run:
+        spots = make_board()
+        clock.tick(FPS)
+        Piece_Location_Test(spots)
+
 
 def  Piece_Location(x, y, spots_Dict):
     xy_range = 25
@@ -16,9 +26,11 @@ def Piece_Location_Test(spots_Dict):
     
     for x in range(800):
         for y in range(800):
-            for i in spots_Dict.values():
+            for is in spots_Dict.values():
                 if (i[0] > x - xy_range and i[0] < x + xy_range and i[1] > y - xy_range and i[1] < y + xy_range):
                     assert Piece_Location(x, y, spots_Dict) == True
+                else:
+                    assert Piece_Location(x, y, spots_Dict) == False
                     
     print("End")
     return 
