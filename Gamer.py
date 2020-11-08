26a1cca335cf9a842e92404a6186835c42b88fef
elizabeth-nastoff <46881533+elizabeth-nastoff@users.noreply.github.com>
2020-11-08 15:31:05 -0600

    changes from liz and marley

--- Gamer.py
+++ Gamer.py
@@ -1,7 +1,24 @@
+from Piece import Piece 
 
 class Gamer:
-    def __inti__(self, piece_name, piece_color, piece_turn = True):
-        self.piece_turn = piece_turn
-        self.piece_name = piece_name
-        self.piece_color = piece_color
+    def init(self, player_number, piece_color):
+        self.player_num = player_number
+        self.color = piece_color
+        self.piece_list = []
+        self.pieces = 0
+
+    def create_piece(self, piece_number, x, y):
+        self.piece_list[piece_number] = Piece(self.color, x, y)
+        self.pieces = self.pieces + 1
+
+    def kill_piece(self, piece_number):
+        self.piece_list[piece_number].kill()
+        self.pieces = self.pieces - 1
         
+# past attempts??
+# not needed
+#class Gamer:
+#    def __inti__(self, piece_name, piece_color, piece_turn = True):
+#        self.piece_turn = piece_turn
+#        self.piece_name = piece_name
+#        self.piece_color = piece_color
--- Piece.py
+++ Piece.py
@@ -1,3 +1,28 @@
+
+
+"""Piece creation"""
+
+class Piece:
+    #color attribute
+    def init(self, color, x, y):
+        self.color = color
+        self.location_x = x
+        self.location_y = y
+        self.is_alive = True
+
+    def str(self):
+        return self.color[0]
+
+    def update_location(self, x,y):
+        self.location_x = x
+        self.location_y = y
+
+    def kill(self):
+        self.is_alive = False
+
+# past attempts??
+# not needed???
+
 # class Piece:
 #     def __init__(self, row, col, color):
 #         self.row = row
@@ -6,10 +31,9 @@
 #         if self.color == WHITE:
 #             self.direction = 1
         
-"""Piece creation"""
-class Piece:
-    #color attribute
-    def __init__(self, color):
-        self.color = color
-    def __str__(self):
-        return self.color[0]
+#class Piece:
+#    #color attribute
+#    def __init__(self, color):
+#        self.color = color
+#    def __str__(self):
+#        return self.color[0]
