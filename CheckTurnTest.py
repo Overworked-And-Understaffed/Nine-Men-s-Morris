8b163a16c2e669f924a61b9bcd651aeef7090471
ruby <rarwxz@mail.umkc.edu>
2020-11-20 14:01:29 -0600

    Test Code Push

--- CheckTurnTest.py
+++ CheckTurnTest.py
@@ -1,18 +1,25 @@
 import unittest
-import Main
 import pygame
+import os
+import sys
+from pygame.locals import *
 import Board
+
+from Globals import *
 
 class CheckTurnTest(unittest.TestCase):
     def test_CheckTurnTest(self):
+        pygame.init()
+        screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        board = Board.Board(screen)
         for player_turn in range(101):
-            Board.turn = player_turn
+            board.turn = player_turn
             if (player_turn % 2 == 0):
                 # assert Player = Player A
-                self.assertEqual(Board.turnCheck(),"BLACK")
+                self.assertEqual(board.turnCheck(),"BLACK")
             else:
                 # assert Player = Player B
-                self.assertEqual(Board.turnCheck(),"WHITE")
+                self.assertEqual(board.turnCheck(),"WHITE")
         print("End")
         return 
                 
--- PieceLocationTest.py
+++ PieceLocationTest.py
@@ -1,6 +1,11 @@
 import unittest
 import pygame
+import os
+import sys
+from pygame.locals import *
 import Board
+
+from Globals import *
 from GameLogic import cordsToNum
 
 class PieceLocationTest(unittest.TestCase):
@@ -12,18 +17,18 @@ class PieceLocationTest(unittest.TestCase):
 
 
     def test_Piece_Location_Test(self):
+        pygame.init()
+        screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        board = Board.Board(screen)
         xy_range = 25
-        
+
         for x in range(800):
             for y in range(800):
-                for i in Board.boardCoords.values():
-                    if (i[0] > x - xy_range and i[0] < x + xy_range and i[1] > y - xy_range and i[1] < y + xy_range):
-                        assert Piece_Location(x, y, Board.boardCoords) == True
+                for i in board.boardCoords:
+                    if i[0] > x - xy_range and i[0] < x + xy_range and i[1] > y - xy_range and i[1] < y + xy_range :
+                        assert self.Piece_Location(x, y, board.boardCoords) == True, ("Issue at", x, y)
                     else:
-                        assert Piece_Location(x, y, Board.boardCoords) == False
-                        
-        print("End")
-        return 
+                        assert self.Piece_Location(x, y, board.boardCoords) == False, ("Issue at", x, y)
 
 if __name__ == '__main__':
     unittest.main()
