6b1f92d2cfd0178a9f026e5e9b8f84570a48335f
kmer102 <jkoni646@gmail.com>
2020-11-20 14:47:43 -0600

    Update PieceLocationTest.py

--- PieceLocationTest.py
+++ PieceLocationTest.py
@@ -19,9 +19,9 @@ class PieceLocationTest(unittest.TestCase):
             for y in range(800):
                 for i in board.boardCoords:
                     if i[0] > x - xy_range and i[0] < x + xy_range and i[1] > y - xy_range and i[1] < y + xy_range :
-                        assert self.Piece_Location(x, y, board.boardCoords) == True#, ("Issue at", x, y)
+                        assert self.Piece_Location(x, y, board.boardCoords)# == True, ("Issue at", x, y)
                     else:
-                        assert self.Piece_Location(x, y, board.boardCoords) == False#, ("Issue at", x, y)
+                        assert self.Piece_Location(x, y, board.boardCoords)# == False, ("Issue at", x, y)
 
 if __name__ == '__main__':
     unittest.main(exit=False)
