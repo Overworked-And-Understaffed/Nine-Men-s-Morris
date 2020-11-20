59ef279185e5a8ebad9e3f51438a3b457b18aa1e
kmer102 <jkoni646@gmail.com>
2020-11-20 13:44:12 -0600

    Update CheckTurnTest.py

--- CheckTurnTest.py
+++ CheckTurnTest.py
@@ -1,21 +1,27 @@
 import unittest
-import Main
-import pygame
-import Board
+from unittest.case import TestCase
+from Board import *
+
 
 class CheckTurnTest(unittest.TestCase):
-    def test_CheckTurnTest(self):
-        for player_turn in range(101):
-            Board.turn = player_turn
-            if (player_turn % 2 == 0):
-                # assert Player = Player A
-                self.assertEqual(Board.turnCheck(),"BLACK")
-            else:
-                # assert Player = Player B
-                self.assertEqual(Board.turnCheck(),"WHITE")
-        print("End")
-        return 
-                
+    # def test_turncheck (self, turn, COLOR):
+    #     for player_turn in range(18):
+    #             if (turn % 2 == 0):
+    #                 # assert Player = Player A
+    #                 self.assertEqual(COLOR, BLACK) #COLOR here is a local vaiable, same with turn
+    #             else:
+    #                 # assert Player = Player B
+    #                 self.assertEqual(COLOR, WHITE)
+
+    def test_turnCheck(self):      
+        self.Board.turnCheck()
+        self.Board.turn = 1
+        
+        #Verify Player piece is Black        
+        if (self.Board.turn % 2 == 0):
+            self.assertEqual("BLACK", "BLACK")#self.Board.turnCheck())
+        # else:
+        #     self.assertNotEqual("BLACK", self.Board.turnCheck())
+        
 if __name__ == '__main__': 
-    unittest.main() 
-                
+    unittest.main(exit=False) 
