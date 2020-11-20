65e1616dd6d65afa2c013ebfcd02a597b8c53c6e
kmer102 <jkoni646@gmail.com>
2020-11-19 22:10:41 -0600

    Update CheckTurnTest.py

--- TestCode/CheckTurnTest.py
+++ TestCode/CheckTurnTest.py
@@ -6,9 +6,11 @@ class CheckTurnTest(unittest.TestCase):
     def CheckTurnTest (self, turn, color):
         for player_turn in range(101):
                 if (turn % 2 == 0):
+                    
                     # assert Player = Player A
                     self.assert COLOR == BLACK #Color here is a local vaiable, same with turn
                 else:
+                    
                     # assert Player = Player B
                     self.assert COLOR == WHITE
                 
